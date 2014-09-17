1cde615bfb32cd81c500aa7f4cae8c72526e675c
Sergei Vorobev <xvorsx@gmail.com>
2014-09-17 00:44:51 -0700

    Add more resources. Nothing works so far

--- Minecraft/Minecraft.ps1
+++ Minecraft/Minecraft.ps1
@@ -1,6 +1,12 @@
 Configuration MinecraftServer
 {
+    $root = "C:\Minecraft"
+    $serverJarName = "server.jar"
+    $serverJarPath = Join-Path $root $serverJarName
+
     Import-DSCResource -module Choco
+    Import-DscResource -Module xPSDesiredStateConfiguration
+
     ChocoExe choco
     {
         Ensure = "Present"
@@ -11,5 +17,27 @@ Configuration MinecraftServer
         Name = "jre8"
         Ensure = "Present"
         DependsOn = "[ChocoExe]choco"
+    }
+
+    File RootFolder
+    {
+        DestinationPath = $root
+        Type = "Directory"
+    }
+
+    xRemoteFile MinecraftServerJar
+    {
+        Uri = "https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar"
+        DestinationPath = $serverJarPath
+        DependsOn = "[File]RootFolder"
+    }
+
+    WindowsProcess ServerProcess
+    {
+        Path = "C:\ProgramData\Oracle\Java\javapath\java.exe"
+        Arguments = "-Xmx1024M -Xms1024M -jar $serverJarPath nogui"
+        Ensure = "Present"
+        WorkingDirectory = $root
+        DependsOn = @("[xRemoteFile]MinecraftServerJar")
     }
 }
